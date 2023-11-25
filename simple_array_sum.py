4447fc638eabece7170be6d5f44f59ff1f092d51
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-11-25 16:11:54 +0100

    Sum of elements in an array

---
+++ simple_array_sum.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from typing import List
+
+
+def simpleArraySum(array: List[int]) -> int:
+    result: int = 0
+    for number in array:
+        result += number
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
